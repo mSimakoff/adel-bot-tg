5d1b868f45211dc255f2181113582612b40a11f3
Mikhail Simakoff <40891414+mSimakoff@users.noreply.github.com>
2019-12-05 21:06:32 +0300

    Update adel-bot.py

--- adel-bot.py
+++ adel-bot.py
@@ -19,6 +19,7 @@ bot = telebot.TeleBot('1001294980:AAHK0Ich3vr7dJ8i7p3PGZwpYSBP1Tisbj8')
 def make_url():
     return "http://wttr.in/Нижний_Новгород"
 
+
 def process_coin():
     coin = random.randrange(0, 3)
     if coin == 0:
@@ -27,6 +28,7 @@ def process_coin():
         return 'Решка!'
     else:
         return random.choice(['Зависла в воздухе', 'Закатилась за угол', 'Ребро!', 'Тебе так нужен этот выбор?'])
+
 
 def make_parameters():
     params = {
@@ -63,15 +65,15 @@ def get_text_messages(message):
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Я могу много всего, но пока я в разработке ты можешь узнать погоду "
                                                "(/weather) или подбросить монетку (/coin)")
-    elif message.text == "How are U":
+    elif message.text == "How are U" or "Как дела" or "Как дела?":
         answer = how_are_you()
         bot.send_message(message.from_user.id, answer)
     elif message.text == "Подбрось монетку" or "/coin":
         answer = process_coin()
         bot.send_message(message.from_user.id, answer)
-    elif message.text == "/weather":
+    elif message.text == "/weather" or "Как погода?" or "Погода" or "Сегодня будет дождь?" or "Сегодня будет снег?":
         city = "Нижний Новгород"
-        #weather = what_weather(city)
+        # weather = what_weather(city)
         bot.send_message(message.from_user.id, what_weather())
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help или /weather")
