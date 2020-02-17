25f4cb9a94a8ded6f2b4276ff8a8e950eb226bd7
Mikhail Simakoff <40891414+mSimakoff@users.noreply.github.com>
2020-02-17 16:35:47 +0300

    Update adel-bot.py

--- adel-bot.py
+++ adel-bot.py
@@ -22,7 +22,7 @@ bot = telebot.TeleBot('1001294980:AAHK0Ich3vr7dJ8i7p3PGZwpYSBP1Tisbj8')
 
 
 def make_url(city):
-    return f"http://wttr.in/{city}"
+    return 'http://wttr.in/' + city
 
 
 def process_coin():
@@ -33,6 +33,12 @@ def process_coin():
         return 'Решка!'
     else:
         return random.choice(['Зависла в воздухе', 'Закатилась за угол', 'Ребро!', 'Тебе не нужен этот выбор, поверь)'])
+
+def tickers(price):
+    first_full_price = (price*1.03)
+    profit = 10 # in rubles
+    
+
 
 
 def make_parameters():
@@ -65,21 +71,21 @@ def how_are_you():
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Hello":
+    message = message.lower()
+    if message == "hello":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Я могу много всего, но пока я в разработке ты можешь узнать погоду "
-                                               "(/weather) или подбросить монетку (/coin)")
-    elif message.text == "How are U":
+    elif message == "/help":
+        bot.send_message(message.from_user.id, "Я могу много всего, просто пни моего создателя @msimakoff")
+    elif message.text == "how are U":
         answer = how_are_you()
         bot.send_message(message.from_user.id, answer)
-    elif message.text == "Как дела":
+    elif message.text == "как дела":
         answer = how_are_you()
         bot.send_message(message.from_user.id, answer)
-    elif message.text == "Как дела?":
+    elif message.text == "как дела?":
         answer = how_are_you()
         bot.send_message(message.from_user.id, answer)
-    elif message.text == "Подбрось монетку":
+    elif message.text == "подбрось монетку":
         answer = process_coin()
         bot.send_message(message.from_user.id, answer)
     elif message.text == "/coin":
@@ -88,24 +94,19 @@ def get_text_messages(message):
     elif message.text == "/weather":
         # weather = what_weather(city)
         bot.send_message(message.from_user.id, what_weather(cityNN))
-    elif message.text == "Как погода?":
+    elif message.text == "как погода?":
         # weather = what_weather(city)
         bot.send_message(message.from_user.id, what_weather(cityNN))
-    elif message.text == "Сегодня будет дождь?":
+    elif message.text == "сегодня будет дождь?":
         # weather = what_weather(city)
         bot.send_message(message.from_user.id, what_weather(cityNN))
-    elif message.text == "Сегодня будет снег?":
+    elif message.text == "сегодня будет снег?":
         # weather = what_weather(city)
         bot.send_message(message.from_user.id, what_weather(cityNN))
+    elif message.text.find("акци") != -1:
+        bot.send_message(message.from_user.id, "Скоро я смогу рассчитывать акции для тебя")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help или /weather")
-
-
-def process_weather(message):
-    token = message.split(': ')
-    if token[0] == 'Какая погода в городе':
-        city = token[1]
-        what_weather(city)
 
 
 # bot.polling(none_stop=True, interval=1)
@@ -116,18 +117,20 @@ def timebrew():
     realtime = dt.datetime.utcnow()
     nowtime = realtime.strftime("%H:%M")
     if nowtime.split(':')[0] == 12:
-        mssg = f'Хватит отдыхать, работай!'
+        mssg = 'Хватит отдыхать, работай!'
         # bot.send_message(message.from_user.id, mssg)
         bot.send_message(message.from_user.id, mssg)
     elif nowtime.split(':')[0] == 14:
-        return f'Прошло два часа, а ты не изменил мир к лучешму'
+        return 'Прошло два часа, а ты не изменил мир к лучешму'
     elif nowtime.split(':')[0] == 16:
-        return f'Если я бы была буратино, то, говоря что ты работаешь, мой нос увеличивался бы до огромных размеров'
+        return 'Если я бы была буратино, то, говоря что ты работаешь, мой нос увеличивался бы до огромных размеров'
     elif nowtime.split(':')[0] == 18:
-        return f'Скажи мне, что полезного ты сделал за этот день?'
+        return 'Скажи мне, что полезного ты сделал за этот день?'
+
 
 
 bot.polling(none_stop=True, interval=5)
+
 
 while (True):
     timebrew()
